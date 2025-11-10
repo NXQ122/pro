8a524fffa4d7425057afe514c9a9523bcf4bae59
Misha ip-511 <konovalenkomihail6@gmail.com>
2025-11-11 02:04:01 +0700

    проверка

---
+++ hom8/main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+        Cache<String> cache = new Cache<>(3);
+
+        cache.add("A");
+        cache.add("B");
+        cache.add("C");
+        System.out.println("После добавления A, B, C: " + cache);
+
+        cache.add("D"); 
+        System.out.println("После добавления D (A удален): " + cache);
+
+        System.out.println("Первый элемент: " + cache.getFirst());
+        System.out.println("Последний элемент: " + cache.getLast());
+        System.out.println("Элемент с индексом 1: " + cache.getItemByIndex(1));
+
+        System.out.println("Есть ли элемент 'B'? " + cache.exists("B"));
+        System.out.println("Удаляем 'B': " + cache.remove("B"));
+        System.out.println("После удаления 'B': " + cache);
+    }
+}
