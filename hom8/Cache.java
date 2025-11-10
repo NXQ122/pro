980aaeaa6186a16a2ae0c5adcd147542bb2c2e79
Misha ip-511 <konovalenkomihail6@gmail.com>
2025-11-11 01:51:38 +0700

    обобщеный дженерик cache

---
+++ hom8/Cache.java
@@ -0,0 +1,21 @@
+
+
+import java.util.LinkedList;
+
+public class Cache<T> {
+    private final int capacity;
+    private final LinkedList<T> list;
+
+    public Cache(int capacity) {
+        this.capacity = capacity;
+        this.list = new LinkedList<>();
+    }
+
+    public void add(T item) {
+        if (list.size() == capacity) {
+            list.removeFirst(); // удаляем самый старый
+        }
+        list.addLast(item); // добавляем новый
+    }
+
+   
